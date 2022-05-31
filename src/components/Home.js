f2f34a4ed0ee8c6155515ecba122d242bdfe9e0d
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-01 01:34:02 +0530

    image slider done overflow-x bug solved

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import ImgSlider from './ImgSlider'
 
 function Home() {
   return (
     <Container>
+    <ImgSlider />
       Home
     </Container>
   )
@@ -12,6 +14,7 @@ function Home() {
 export default Home 
 
 const Container = styled.main`
+  overflow-x: hidden;
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
