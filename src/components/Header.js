3a6e1812f23e3e6c642dfb1ac7e746a35cd1cfc2
Siddharth <officialsiddharthbisht@gmail.com>
2022-05-31 16:50:41 +0530

    started header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+  return (
+    <HeaderContainer>
+    
+    </HeaderContainer>
+  )
+}
+
+export default Header
+
+const HeaderContainer = styled.div`
+    height: 70px;
+    background: #090b13;
+
+`
