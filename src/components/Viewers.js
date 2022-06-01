9af7a5d35084ef93e2da1e466eeaa4f983b72e1b
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-01 12:27:23 +0530

    viewers section finished

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -4,7 +4,21 @@ import styled from 'styled-components'
 function Viewers() {
   return (
     <Container>
-        Viewers
+        <Wrap>
+            <img src='/images/viewers-disney.png'/>
+        </Wrap>
+        <Wrap>
+            <img src='/images/viewers-pixar.png'/>
+        </Wrap>
+        <Wrap>
+            <img src='/images/viewers-marvel.png'/>
+        </Wrap>
+        <Wrap>
+            <img src='/images/viewers-starwars.png'/>
+        </Wrap>
+        <Wrap>
+            <img src='/images/viewers-national.png'/>
+        </Wrap>
     </Container>
 
   )
@@ -14,6 +28,24 @@ export default Viewers
 
 const Container = styled.div`
     margin-top : 30px;
-    ${'' /* display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr)); */}
+    display: grid;
+    padding: 30px 0px 26px;
+    grid-template-columns: repeat(5,minmax(0,1fr));
+    gap: 25px;
+`
+const Wrap = styled.div`
+  border-radius: 10px;
+  cursor: pointer;
+  border : 3px solid rgba(249,249,249,0.1);
+  box-shadow : rgb(0 0 0 / 69%) 0px 26px 30px -10px , rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  &:hover{
+    box-shadow:rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72) 0px 30px 22px -10px;
+    transform: scale(1.05);
+    border-color: rgba(249,249,249,0.8);
+  }
 `
