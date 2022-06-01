81b248266120543896cd16165dd1f72a7d9ba858
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-01 13:48:59 +0530

    started detail page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import Home  from './components/Home';
-
+import Detail from './components/Detail';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
