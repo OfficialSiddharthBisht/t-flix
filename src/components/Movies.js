b7996ff3df42440819a3f5ebedcd1778983b1a7d
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-01 13:24:13 +0530

    Movies section design done

---
+++ src/components/Movies.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Movies() {
+  return (
+    <Container>
+        <h4>Recommended for You</h4>
+        <Content>
+            <Wrap>
+                <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.7nCZmDR-eceevGpLTYZynQHaEK%26pid%3DApi&f=1'/>
+            </Wrap>
+            <Wrap>
+                <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.7nCZmDR-eceevGpLTYZynQHaEK%26pid%3DApi&f=1'/>
+            </Wrap>
+            <Wrap>
+                <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.7nCZmDR-eceevGpLTYZynQHaEK%26pid%3DApi&f=1'/>
+            </Wrap>
+            <Wrap>
+                <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.7nCZmDR-eceevGpLTYZynQHaEK%26pid%3DApi&f=1'/>
+            </Wrap>
+            <Wrap>
+                <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.7nCZmDR-eceevGpLTYZynQHaEK%26pid%3DApi&f=1'/>
+            </Wrap>
+            <Wrap>
+                <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.7nCZmDR-eceevGpLTYZynQHaEK%26pid%3DApi&f=1'/>
+            </Wrap>
+            <Wrap>
+                <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.7nCZmDR-eceevGpLTYZynQHaEK%26pid%3DApi&f=1'/>
+            </Wrap>
+            <Wrap>
+                <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.7nCZmDR-eceevGpLTYZynQHaEK%26pid%3DApi&f=1'/>
+            </Wrap>
+        </Content>
+    </Container>
+  )
+}
+
+export default Movies
+
+const Container = styled.div`
+    
+`
+const Content = styled.div`
+    display : grid;
+    gap : 25px;
+    grid-template-columns : repeat(4, minmax(0, 1fr));
+`
+const Wrap = styled.div`
+    cursor : pointer;
+    border-radius : 15px;
+    overflow: hidden;
+    border : 3px solid rgba(249 , 249 , 249 , 0.1);
+    box-shadow : rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition : all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+    img{
+        width : 100%;
+        height : 100%;
+        object-fit : cover;
+    }
+    &:hover{
+        transform : scale(1.05);
+        border-color: rgba(249 , 249 , 249 , 0.8);
+        box-shadow : rgb(0 0 0 / 80%) 0ox 40px 58px -16px , rgb(0 0 0 /72%) 0px 30px 22px -10px;
+    }
+`
